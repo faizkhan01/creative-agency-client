6b678872d7543f9b612fab684546c56069f87114
faizkhan01 <67521965+faizkhan01@users.noreply.github.com>
2020-10-16 04:24:47 +0600

    creative-agency client site coded successfully Alhamdulillah

---
+++ src/Components/LoginPage/fireConfig.js
@@ -0,0 +1,11 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyBs4prX3xKrCGCRW4m5dPMt7HMUWHiI1MY",
+  authDomain: "creative-agency-explore.firebaseapp.com",
+  databaseURL: "https://creative-agency-explore.firebaseio.com",
+  projectId: "creative-agency-explore",
+  storageBucket: "creative-agency-explore.appspot.com",
+  messagingSenderId: "764844211585",
+  appId: "1:764844211585:web:fd1286af861e62f817bc80"
+};
+
+  export default firebaseConfig;
